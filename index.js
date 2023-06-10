99a592a72f0c2cb15b1d228847683edc4cfe0b0f
ReeceRenninger <renningerreece@gmail.com>
2023-06-10 14:00:41 -0700

    setup

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ require('dotenv').config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // enable the following to use socket mode
+  appToken: process.env.APP_TOKEN,
 });
+
+
+app.command('/knowledge', async ({ command, ack, say }) => {
+  try {
+    await ack();
+    say('Yaaay! that command works!');
+  } catch (error) {
+    console.log('err');
+    console.error(error);
+  }
+});
+
 
 (async () => {
   const port = process.env.PORT || 3002;
@@ -15,3 +29,4 @@ const app = new App({
   await app.start(process.env.PORT || port);
   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
 })();
+
