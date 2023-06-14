9ccf7e5950d9d1edade3640d8796947148393cd5
Donna Ada <donnaada@icloud.com>
2023-06-14 11:05:25 +1000

    working aws deployment; still have `say` bug in chatModule

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { App } = require('@slack/bolt');
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
 require('dotenv').config();
 // require the fs module that's built into Node.js
 const fs = require('fs');
@@ -13,17 +13,22 @@ const faqModule = require('./src/modules/faqModule');
 const updateModule = require('./src/modules/updateModule');
 const chatModule = require('./src/modules/chatModule');
 
+const awsLambdaReceiver = new AwsLambdaReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, // enable the following to use socket mode
-  appToken: process.env.APP_TOKEN,
+  receiver: awsLambdaReceiver,
+  // socketMode: true, // enable the following to use socket mode
+  // appToken: process.env.APP_TOKEN,
 });
 
 //!! PROOF OF LIFE MESSAGE
 app.command('/hello', async ({ command, ack, say }) => {
   try {
+    console.log('command>>>>>', command);
     await ack();
     say('I am alive!!!');
   } catch (error) {
@@ -54,10 +59,16 @@ updateModule(app, faqs);
 chatModule(app);
 
 
-//start up our bot
-(async () => {
-  const port = process.env.PORT || 3002;
-  // Start your app
-  await app.start(process.env.PORT || port);
-  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
-})();
+// //start up our bot
+// (async () => {
+//   const port = 'https://chatgptbotbackend.onrender.com';
+//   // Start your app
+//   await app.start(port);
+//   console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+// })();
+
+// Handle the Lambda function event
+module.exports.handler = async (event, context, callback) => {
+  const handler = await awsLambdaReceiver.start();
+  return handler(event, context, callback);
+}
